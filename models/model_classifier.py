48b26957f4472b072d852b61bd4503f5c08836fa
Hannes Steiner <hannes.steiner@web.de>
2025-04-29 23:07:26 +0200

    Add ESC50_CNN

--- models/model_classifier.py
+++ models/model_classifier.py
@@ -75,3 +75,41 @@ class MFCC_10CNN(nn.Module):
         x = F.adaptive_avg_pool1d(x, 1).reshape(x.shape[0], -1)
         x = self.classifier(x)
         return x
+
+
+class ESC50_CNN(nn.Module):
+    def __init__(self, n_mels, n_frames, num_classes=50):
+        super().__init__()
+
+        def conv_block(in_channels, out_channels):
+            return nn.Sequential(
+                nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
+                nn.BatchNorm2d(out_channels),
+                nn.ReLU(inplace=True),
+                nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1),
+                nn.BatchNorm2d(out_channels),
+                nn.ReLU(inplace=True),
+                nn.MaxPool2d(2)
+            )
+
+        self.features = nn.Sequential(
+            conv_block(1, 32),
+            conv_block(32, 64),
+            conv_block(64, 128),
+            conv_block(128, 256),
+        )
+
+        pooled_mels = n_mels // 16
+        pooled_frames = n_frames // 16
+        self.classifier = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(256 * pooled_mels * pooled_frames, 512),
+            nn.ReLU(inplace=True),
+            nn.Dropout(0.5),
+            nn.Linear(512, num_classes)
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        x = self.classifier(x)
+        return x
