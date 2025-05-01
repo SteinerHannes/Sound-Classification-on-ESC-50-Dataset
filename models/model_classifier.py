46e7f9ce8ffbdd77551f1ee0df19d6283ce50793
Hannes Steiner <hannes.steiner@web.de>
2025-05-01 21:30:12 +0200

    007

--- models/model_classifier.py
+++ models/model_classifier.py
@@ -84,27 +84,27 @@ class ESC50_CNN(nn.Module):
         self.features = nn.Sequential(
             nn.Conv2d(1, 32, kernel_size=5, padding=1),
             nn.BatchNorm2d(32),
-            nn.ReLU(),
+            nn.LeakyReLU(),
             nn.MaxPool2d(kernel_size=2),
 
             nn.Conv2d(32, 64, kernel_size=5, padding=1),
             nn.BatchNorm2d(64),
-            nn.ReLU(),
+            nn.LeakyReLU(),
             nn.MaxPool2d(kernel_size=2),
 
             nn.Conv2d(64, 128, kernel_size=5, padding=1),
             nn.BatchNorm2d(128),
-            nn.ReLU(),
+            nn.LeakyReLU(),
             nn.AvgPool2d(kernel_size=2),
 
             nn.Conv2d(128, 256, kernel_size=5, padding=1),
             nn.BatchNorm2d(256),
-            nn.ReLU(),
+            nn.LeakyReLU(),
             nn.AvgPool2d(kernel_size=2),
 
             nn.Conv2d(256, 512, kernel_size=5, padding=1),
             nn.BatchNorm2d(512),
-            nn.ReLU(),
+            nn.LeakyReLU(),
             nn.Dropout(0.3),
             nn.AdaptiveMaxPool2d((1, 1)),
         )
