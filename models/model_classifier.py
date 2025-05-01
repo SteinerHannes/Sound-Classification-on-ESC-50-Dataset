0496fa8781d8e3c95d6f79d6cd3640dded68d877
Hannes Steiner <hannes.steiner@web.de>
2025-05-01 19:25:10 +0200

    005

--- models/model_classifier.py
+++ models/model_classifier.py
@@ -82,27 +82,27 @@ class ESC50_CNN(nn.Module):
 
 
         self.features = nn.Sequential(
-            nn.Conv2d(1, 32, kernel_size=7, padding=3),
+            nn.Conv2d(1, 32, kernel_size=5, padding=2),
             nn.BatchNorm2d(32),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2),
 
-            nn.Conv2d(32, 64, kernel_size=7, padding=3),
+            nn.Conv2d(32, 64, kernel_size=5, padding=1),
             nn.BatchNorm2d(64),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2),
 
-            nn.Conv2d(64, 128, kernel_size=5, padding=2),
+            nn.Conv2d(64, 128, kernel_size=5, padding=1),
             nn.BatchNorm2d(128),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2),
 
-            nn.Conv2d(128, 256, kernel_size=5, padding=2),
+            nn.Conv2d(128, 256, kernel_size=5, padding=1),
             nn.BatchNorm2d(256),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2),
 
-            nn.Conv2d(256, 512, kernel_size=3, padding=1),
+            nn.Conv2d(256, 512, kernel_size=5, padding=1),
             nn.BatchNorm2d(512),
             nn.ReLU(),
             nn.Dropout(0.3),
