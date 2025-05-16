c7bb67f2cc69aa4e246cb0f4c9cc56354ed4ebf6
Hannes Steiner <hannes.steiner@web.de>
2025-05-16 15:03:03 +0200

    Update code

--- dataset/dataset_ESC50.py
+++ dataset/dataset_ESC50.py
@@ -53,7 +53,7 @@ def download_progress(current, total, width=80):
 
 class ESC50(data.Dataset):
 
-    def __init__(self, root, cfg:DictConfig, test_folds=frozenset((1,)), subset="train", global_mean_std=(0.0, 0.0), download=False):
+    def __init__(self, root, cfg:DictConfig, test_folds=frozenset((1,)), subset="train", global_mean_std=(0.0, 1.0), download=False):
         self.cfg = cfg
         audio = 'ESC-50-master/audio'
         root = os.path.normpath(root)
